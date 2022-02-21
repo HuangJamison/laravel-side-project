996761c2e2f20c3effbf8d3a2e731e861894b643
J米 <b10130402@gmail.com>
2022-02-21 22:32:51 +0800

    feat: webpack css/js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('resources/js/app.js', 'public/js');
+
+mix.styles([
+    'resources/css/todo/all.css',
+    'resources/css/todo/create.css',
+], 'public/css/todo.css');
